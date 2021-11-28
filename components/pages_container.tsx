51d49b2d1e3cfe2a7d29e834347454fda42b843e
Marc Espín Sanz <mespinsanz@gmail.com>
2021-11-28 13:45:39 +0100

    :bug: Bug fixes

--- components/pages_container.tsx
+++ components/pages_container.tsx
@@ -5,7 +5,7 @@ const PagesContainer = styled('div', {
     height: '100vh',
     overflow: 'auto',
     flex: 1,
-    transition: '1s ease-in-out',
+    transition: 'opacity 1s ease-in-out',
     '&.hidden': {
         transition: '0.05s ease-in-out',
         opacity: 0
