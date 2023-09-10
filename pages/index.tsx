370e0127d13042908c61e36af2e5eeb86f53bb64
marc2332 <mespinsanz@gmail.com>
2023-09-10 18:05:38 +0200

    more tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home() {
             </CircularCard>
           </div>
         </div>
-        <p className="flex-1 leading-8 float-left">
+        <p className="flex-1 leading-8 float-left mb-6">
           <Image
             className="rounded-full float-right"
             alt="Profile Picture"
