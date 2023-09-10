0acf2387cf8a07fd61dddbb7ecf5891a0169cca4
marc2332 <mespinsanz@gmail.com>
2023-09-10 22:48:07 +0200

    tweak

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="p-10 md:w-9/12 lg:w-1/2 mx-auto">
+    <div className="p-10 md:w-9/12 lg:w-1/2 mx-auto sm:translate-x-[-40px]">
       {children}
     </div>
   );
