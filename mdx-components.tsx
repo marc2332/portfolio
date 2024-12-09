daebc43aaa9348276e2e79ea7c35b0fa32a408b9
marc2332 <mespinsanz@gmail.com>
2024-12-09 16:38:20 +0100

    add @types/mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,5 @@
-import type { MDXComponents } from 'mdx/types'
- 
+import { MDXComponents } from "mdx/types";
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         h1: ({ children }) => {
